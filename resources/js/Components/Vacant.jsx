8418d016816b0e73c648cbfeb2de320fd13058dc
YannickNQ3 <yanaxnq3@gmail.com>
2023-09-10 18:39:31 -0500

    Fixing errors

--- resources/js/Components/Vacant.jsx
+++ resources/js/Components/Vacant.jsx
@@ -1,109 +1,137 @@
-import React, { useState } from 'react';
-import Dropdown from '@/Components/Dropdown';
-import InputError from '@/Components/InputError';
-import PrimaryButton from '@/Components/PrimaryButton';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import { useForm, usePage } from '@inertiajs/react';
-import Modal from '@/Components/Modal';
- 
+import { useForm, usePage } from "@inertiajs/react";
+import React, { useState } from "react";
+import PrimaryButton from "./PrimaryButton";
+import Modal from "./Modal";
+
 export default function Vacant({ vacant }) {
+
+    // Variables
+
     const { auth } = usePage().props;
- 
+
     const [editing, setEditing] = useState(false);
 
     const [showMore, setShowMore] = useState(false);
 
-    const [buttonText, setButtonText] = useState('Ver resumen');
+    const [ showModal, setShowModal ] = useState(false);
 
-    const [showModal, setShowModal ] = useState(false);
-    
-    const handleOpenModal = () => {
+    const [buttonText, setButtonText] = useState("Ver resumen");
+
+    // Data
+    const { data, setData, patch, clearErrors, reset, errors, delete: destroy } = useForm({
+        area: vacant.area,
+        title: vacant.title,
+        description: vacant.description,
+        date_end: vacant.date_end,
+    }); 
+
+    // Handlers
+
+    const showInfo = () => {
+        setShowMore(!showMore);
+        setButtonText(showMore ? "Ver resumen" : "Cerrar");
+    };
+
+    const openModal = () => {
         setEditing(true);
         setShowModal(true);
     };
 
-    const handleCloseModal = () => {
+    const closeModal = () => {
         setEditing(false);
         setShowModal(false);
         reset();
         clearErrors();
     };
- 
-    const { data, setData, patch, clearErrors, reset, errors } = useForm({
-        area: vacant.area,
-        title: vacant.title,
-        description: vacant.description,
-        date_end: vacant.date_end,
-    });
 
-    const handleShowInfo = () => {
-        setShowMore(!showMore);
-        setButtonText(showMore? 'Ver resumen' : 'Cerrar');
-    };
+    // Submit
 
     const submit = (e) => {
         e.preventDefault();
-        patch(route('vacants.update', vacant.id), { onSuccess: () => setEditing(false) });
+        patch(route('vacants.update', vacant.id), { onSuccess: () => setEditing(false)});
     };
+    
+    const deleteVacant = (e) => {
+        e.preventDefault();
+        destroy(route('vacants.destroy', vacant.id));
+    }
 
     return (
-        <tr className="bg-white border-b">
-            <td className="p-4">
-                <p className='text-sm font-semibold'>{data.area}</p>
-                {showMore &&
-                    <div className='text-sm pt-2'> 
-                        <p className='uppercase'>{data.title}</p>
-                        <p className='text-justify pt-2'>{data.description}</p>
-                    </div>
-                }
-                <button className='flex-1 pt-2 text-sm text-red-600 underline' onClick={handleShowInfo}>{buttonText}</button>
-                <span className='pl-2'>|</span>
-                <button className='flex-1 pl-2 pt-2 text-sm text-red-600 underline'>Postular</button>
-                
+        <tr className="text-sm">
+            <td className="pt-2">
+                <p className="font-semibold">{vacant.area}</p>
+                {showMore && (
+                    <>
+                        <p className="uppercase">{vacant.title}</p>
+                        <p className="text-justify">{vacant.description}</p>
+                    </>
+                )}
+                <button
+                    className="text-red-600 underline"
+                    onClick={showInfo}
+                >
+                    {buttonText}
+                </button>
+                <span className="font-bold"> | </span>
+                <button className="text-red-600 underline">Postular</button>
             </td>
-            <td className="text-sm text-center">{data.date_end}</td>
-
-            <td className="text-sm text-center">
-                <div className='flex'>
-                    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-lime-500 dark:hover:bg-lime-400 dark:focus:ring-blue-800" onClick={() => handleOpenModal()}>
-                        <svg className="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                            <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"/>
-                            <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z"/>
+            <td className="text-center pt-2">{vacant.date_end}</td>
+            <td>
+                <div className="h-auto flex items-center justify-center text-center pt-2 space-x-2">
+                    <PrimaryButton className="bg-green-700 after:content-[''] md:after:content-['Editar']" onClick={() => openModal()}>
+                        <svg
+                            className="w-3.5 h-3.5 md:mr-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 18 21"
+                        >
+                            <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z" />
+                            <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z" />
                         </svg>
-                        Editar
-                    </button>
-
-                    <button href={route('vacants.destroy', vacant.id)} method="delete" type="button" className="text-white bg-red-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-500 dark:focus:ring-red-800">
-                        <svg className="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
+                    </PrimaryButton>
+                    <PrimaryButton as="button" onClick = {deleteVacant} method="delete" className="bg-red-800 after:content-[''] md:after:content-['Eliminar']">
+                        <svg
+                            className="w-3.5 h-3.5 md:mr-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 18 21"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+                            />
                         </svg>
-                        Eliminar
-                    </button>
+                    </PrimaryButton>
                 </div>
-            </td>
-            { editing ? showModal &&
-                <Modal show={showModal} onClose={handleCloseModal}>
-                <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-                    <form onSubmit={submit}>
-                        <h2 className="text-center pb-2 text-gray-800 font-bold uppercase text-lg">Agregar Vacante</h2>
-                        <label className="font-medium mb-2 flex">Area</label>
-                        <input value={data.area} type="text" name="area" placeholder="Área"  onChange= { e => setData('area', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
-                        <label className="font-medium mb-2 flex">Titulo</label>
-                        <input value={data.title} type="text" name="title" placeholder="Título" onChange= { e => setData('title', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
-                        <label className="font-medium mb-2 flex">Descripción</label>
-                        <textarea value={data.description} name="description" placeholder="Descripción" onChange= { e => setData('description', e.target.value)} className="w-full px-4 py-2 h-32 mb-4 border border-blue-300 rounded-lg"></textarea>
-                        <label className="font-medium mb-2 flex">Fecha Limite</label>
-                        <input value={data.date_end} type="date" name="date_end" onChange= { e => setData('date_end', e.target.value)} placeholder="Fecha de finalización" className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
-                        <div className="flex justify-end mt-4">
-                            <PrimaryButton className="mt-4" >Guardar</PrimaryButton>
-                            <PrimaryButton className="mt-4 ml-4 bg-red-700" onClick = {handleCloseModal}>Cancelar</PrimaryButton>
+                {editing ? 
+                    showModal &&
+                    <Modal show={showModal} onClose={closeModal}>
+                        <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                            <form onSubmit={submit}>
+                                <h2 className="text-center pb-2 text-gray-800 font-bold uppercase text-lg">Editar Vacante</h2>
+                                <label className="font-medium mb-2 flex">Area</label>
+                                <input value={data.area} type="text" name="area" placeholder="Área"  onChange= { e => setData('area', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
+                                <label className="font-medium mb-2 flex">Titulo</label>
+                                <input value={data.title} type="text" name="title" placeholder="Título" onChange= { e => setData('title', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
+                                <label className="font-medium mb-2 flex">Descripción</label>
+                                <textarea value={data.description} name="description" placeholder="Descripción" onChange= { e => setData('description', e.target.value)} className="w-full px-4 py-2 h-32 mb-4 border border-blue-300 rounded-lg"></textarea>
+                                <label className="font-medium mb-2 flex">Fecha Limite</label>
+                                <input value={data.date_end} type="date" name="date_end" onChange= { e => setData('date_end', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
+                                <div className="flex justify-end mt-4">
+                                    <PrimaryButton type="submit" className="mt-4" >Guardar</PrimaryButton>
+                                    <PrimaryButton type="button" className="mt-4 ml-4 bg-red-700" onClick = {closeModal}>Cancelar</PrimaryButton>
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                </div>
-            </Modal>
-            : showModal 
-            }
+                    </Modal>
+                    : showModal
+                }
+            </td>
         </tr>
-    )
+    );
 }
--- resources/js/Pages/Vacant/Index.jsx
+++ resources/js/Pages/Vacant/Index.jsx
@@ -1,13 +1,13 @@
-import React, {useState} from "react";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import InputError from "@/Components/InputError";
+import Modal from "@/Components/Modal";
+import Vacant from "@/Components/Vacant";
 import PrimaryButton from "@/Components/PrimaryButton";
-import { useForm, Head} from '@inertiajs/react';
-import Vacant from '@/Components/Vacant';
-import Modal from '@/Components/Modal';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, useForm } from "@inertiajs/react";
+import React, { useState } from "react";
 
-export default function Index({ auth, vacants}){
-
+export default function Index( {auth, vacants})
+{
+    /* Modal */
     const [showModal, setShowModal ] = useState(false);
     
     const handleOpenModal = () => {
@@ -16,7 +16,6 @@ export default function Index({ auth, vacants}){
 
     const handleCloseModal = () => {
         reset();
-        clearErrors();
         setShowModal(false);
     };
 
@@ -33,56 +32,52 @@ export default function Index({ auth, vacants}){
             handleCloseModal();
         }});
     };
-
-    return (
-        <AuthenticatedLayout user = { auth.user }>
+    
+    return(
+        <AuthenticatedLayout user = {auth.user}>
             <Head title="Trabajos Disponibles" />
-        
-        <div className="flex-wrap-reverse justify-center max-w-7xl px-4 mx-auto">
-
-            <div className="flex justify-end box-content p-4 ">
-                <PrimaryButton onClick={handleOpenModal}>Agregar Vacante</PrimaryButton>
-            </div>            
-
+            <div className="flex-wrap-reverse justify-center max-w-7xl px-4 mx-auto">
+                <div className="flex justify-end">
+                    <PrimaryButton className="m-4" onClick={handleOpenModal}>Agregar Vacante</PrimaryButton>
+                </div>
+                <div className="bg-white shadow-sm rounded-lg box-content w-auto p-4">
+                    <table className="w-full h-auto">
+                        <thead className="bg-gray-50 border-b-2 border-gray-200">
+                            <tr>
+                                <th className="rounded-lg p-4 w-auto text-base font-bold tracking-wide text-gray-800 text-left">Ofertas de Trabajo</th>
+                                <th className="bg-gray-50 w-52 text-base border-b-2 border-gray-200 text-gray-800 text-center">Fecha Limite</th>
+                                <th className="bg-gray-50 w-52 text-base border-b-2 border-gray-200 text-gray-800 text-center">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {vacants.map( vacant => 
+                                <Vacant key={vacant.id} vacant={vacant}></Vacant>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
             {showModal && 
                 <Modal show={showModal} onClose={handleCloseModal}>
-                    <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                    <div className="max-w-2xl mx-auto p-6 sm:p-4 lg:p-8">
                         <form onSubmit={submit}>
                             <h2 className="text-center pb-2 text-gray-800 font-bold uppercase text-lg">Agregar Vacante</h2>
-                            <label className="font-medium mb-2 flex">Area</label>
-                            <input value={data.area} type="text" name="area" placeholder="Área"  onChange= { e => setData('area', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
-                            <label className="font-medium mb-2 flex">Titulo</label>
-                            <input value={data.title} type="text" name="title" placeholder="Título" onChange= { e => setData('title', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
-                            <label className="font-medium mb-2 flex">Descripción</label>
-                            <textarea value={data.description} name="description" placeholder="Descripción" onChange= { e => setData('description', e.target.value)} className="w-full px-4 py-2 h-32 mb-4 border border-blue-300 rounded-lg"></textarea>
-                            <label className="font-medium mb-2 flex">Fecha Limite</label>
-                            <input value={data.date_end} type="date" name="date_end" onChange= { e => setData('date_end', e.target.value)} placeholder="Fecha de finalización" className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg" />
-                            <div className="flex justify-end mt-4">
-                                <PrimaryButton className="mt-4" disabled={processing}>Aceptar</PrimaryButton>
-                                <PrimaryButton className="mt-4 ml-4 bg-red-700" onClick = {handleCloseModal}>Cancelar</PrimaryButton>
+                            <label htmlFor='area' className="flex font-medium mb-2">Area</label>
+                            <input type="text" id='area' name="area" placeholder="Area" value={data.area} onChange={ e => setData('area', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg"/>
+                            <label htmlFor='title' className="flex font-medium mb-2">Titulo</label>
+                            <input type="text" id='title' name="title" placeholder="Titulo" value={data.title} onChange={ e => setData('title', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg"/>
+                            <label htmlFor='description' className="flex font-medium mb-2">Descripción</label>
+                            <textarea id='description' name="description" placeholder="Descripción" value={data.description} onChange={ e => setData('description', e.target.value)} className="w-full px-4 py-2 h-32 mb-4 border border-blue-300 rounded-lg"></textarea>
+                            <label htmlFor='date_end' className="flex font-medium mb-2">Fecha Limite</label>
+                            <input type="date" id='date_end' name="date_end" value={data.date_end} onChange={ e => setData('date_end', e.target.value)} className="w-full px-4 py-2 mb-4 border border-blue-300 rounded-lg"/>
+                            <div className="flex justify-end space-x-2">
+                                <PrimaryButton type="submit" disabled={processing} className="">Aceptar</PrimaryButton>
+                                <PrimaryButton type="button" onClick={handleCloseModal} className="bg-red-700">Cancelar</PrimaryButton>
                             </div>
                         </form>
                     </div>
-                </Modal>}
-                
-            <div className="bg-white overflow-hidden shadow-sm rounded-lg flex-1 box-content w-auto p-4 ">
-                <table className="w-full h-auto">
-                    <thead className="bg-gray-50 border-b-2 border-gray-200">
-                        <tr>
-                            <th className="rounded-lg p-4 w-auto min-w-160 text-base font-bold tracking-wide text-gray-800 text-left">Ofertas de Trabajo</th>
-                            <th className="bg-gray-50 w-52 text-base border-b-2 border-gray-200 text-gray-800 text-center">Fecha Limite</th>
-                            <th className="bg-gray-50 w-52 text-base border-b-2 border-gray-200 text-gray-800 text-center">Acciones</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {vacants.map(vacant =>
-                            <Vacant key={vacant.id} vacant={vacant}></Vacant>
-                        )}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                </Modal>
+            }
         </AuthenticatedLayout>
-    );
-};
+    )
+}
