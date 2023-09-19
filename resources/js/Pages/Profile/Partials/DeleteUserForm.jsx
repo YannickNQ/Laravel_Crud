ddef5a14265c5a59d340228cb3ca5607449fddd3
YannickNQ3 <yanaxnq3@gmail.com>
2023-09-19 09:27:26 -0500

    Update forms for profile ops

--- resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
@@ -46,25 +46,24 @@ export default function DeleteUserForm({ className = '' }) {
     return (
         <section className={`space-y-6 ${className}`}>
             <header>
-                <h2 className="text-lg font-medium text-gray-900">Delete Account</h2>
+                <h2 className="text-lg font-medium text-gray-900">Eliminar Cuenta</h2>
 
-                <p className="mt-1 text-sm text-gray-600">
-                    Once your account is deleted, all of its resources and data will be permanently deleted. Before
-                    deleting your account, please download any data or information that you wish to retain.
+                <p className="mt-1 text-sm text-gray-600 text-justify">
+                Una vez que se elimine su cuenta, todos sus recursos y datos se eliminarán permanentemente. Antes de eliminar su cuenta, descargue cualquier dato o información que desee conservar.
                 </p>
             </header>
 
-            <DangerButton onClick={confirmUserDeletion}>Delete Account</DangerButton>
+            <DangerButton onClick={confirmUserDeletion}>Eliminar Cuenta</DangerButton>
 
             <Modal show={confirmingUserDeletion} onClose={closeModal}>
                 <form onSubmit={deleteUser} className="p-6">
                     <h2 className="text-lg font-medium text-gray-900">
-                        Are you sure you want to delete your account?
+                        ¿Estás seguro de eliminar tu cuenta?
                     </h2>
 
                     <p className="mt-1 text-sm text-gray-600">
-                        Once your account is deleted, all of its resources and data will be permanently deleted. Please
-                        enter your password to confirm you would like to permanently delete your account.
+                        Una vez que tu cuentaa sea eliminada, todos sus recursos y datos serán permanentemente eliminados. Por favor,
+                        ingresa tu contraseña para confirmar de que quieres eliminar permanentemente tu cuenta.
                     </p>
 
                     <div className="mt-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,4 @@ export default {
     },
 
     plugins: [forms],
-
-    important: "#app",
 };
