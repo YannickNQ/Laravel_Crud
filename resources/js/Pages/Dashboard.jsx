80adea38aa8082aa995a910ccaae7f892bc3e7a3
ommstock <ommskara@gmail.com>
2023-09-25 10:32:38 -0500

    DatatableMUI

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,41 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+// import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import MUIDataTable from "mui-datatables";
+
+const columns = ["Name", "Company", "City", "State"];
+
+const data = [
+    ["Joe James", "Test Corp", "Yonkers", "NY"],
+    ["John Walsh", "Test Corp", "Hartford", "CT"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL"],
+    ["James Houston", "Test Corp", "Dallas", "TX"],
+];
+
+const options = {
+    filterType: 'checkbox',
+};
 
 export default function Dashboard({ auth }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
+        // <AuthenticatedLayout
+            // user={auth.user}
+            // header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        // >
+            // <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">¡Bienvenido!</div>
+                        {/* <div className="p-6 text-gray-900">¡Bienvenido!</div> */}
+                        <MUIDataTable
+                            title={"Employee List"}
+                            data={data}
+                            columns={columns}
+                            options={options}
+                        />
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        // </AuthenticatedLayout>
     );
 }
