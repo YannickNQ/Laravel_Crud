7e18e2c67a2421b230f778bf44ab3b8da63a9ab6
Yannick Axel Nina Quispe <yninaq@est.unap.edu.pe>
2023-09-26 11:59:11 -0500

    Add Social Login and Register

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -16,7 +16,6 @@ import { useState } from "react";
 
 export default function Login({ status, canResetPassword }) {
 
-    const [showForm, setShowForm] = useState(false);
     const { data, setData, post, processing, errors, reset } = useForm({
         email: "",
         password: "",
@@ -35,133 +34,120 @@ export default function Login({ status, canResetPassword }) {
         post(route("login"));
     };
 
-    const ShowEmailForm = () => {
-        setShowForm(true);
-    }
-
     return (
-        <GuestLayout>
-            <Head title="Log in" />
-            <div className="flex flex-col">
-                <a href="/auth/github/redirect" className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        className="text-black"
-                        type="github"
-                        label="Usar GitHub"
-                    />
-                </a>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+            <CssBaseline />
+                <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: 'url(https://e0.pxfuel.com/wallpapers/149/259/desktop-wallpaper-web-development.jpg)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+                />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <GuestLayout>
+                    <Head title="Log in" />
+                    <div className="flex flex-col">
+                        <a href='/auth/github/redirect' className='flex w-full justify-center items-center'>
+                            <SocialButton
+                                type="github"
+                                label="Usar GitHub"
+                            />
+                        </a>
+                        <a href='/auth/google/redirect' className='flex w-full justify-center items-center'>
+                            <SocialButton
+                                type="google"
+                                label="Usar Google"
+                            />
+                        </a>
+                        <a href='/auth/facebook/redirect' className='flex w-full justify-center items-center'>
+                            <SocialButton
+                                type="facebook"
+                                label="Usar Facebook"
+                            />
+                        </a>
+                    </div>
+                    <span className='flex items-center justify-center mt-2'>ó</span>
 
-                <a href="/auth/google/redirect" className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        className="text-black"
-                        type="google"
-                        label="Usar Google"
-                    />
-                </a>
+                    {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
-                <a href="/auth/facebook/redirect" className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        className="text-black font-normal"
-                        type="facebook"
-                        label="Usar Facebook"
-                    />
-                </a>
-                <a onClick={ShowEmailForm} className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        className="text-black font-normal"
-                        type="email"
-                        label="Usar mi correo"
-                    />
-                </a>
-            </div>
+                    <form onSubmit={submit}>
+                        <div>
+                            <InputLabel htmlFor="email" className="mt-2" value="Correo Electrónico" />
 
-            {status && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    {status}
-                </div>
-            )}
+                            <TextInput
+                                id="email"
+                                type="email"
+                                name="email"
+                                value={data.email}
+                                className="mt-1 block w-full"
+                                autoComplete="username"
+                                isFocused={true}
+                                onChange={(e) => setData('email', e.target.value)}
+                            />
+                            <InputError message={errors.email} className="mt-2" />
+                        </div>
 
-            {showForm &&
+                        <div className="mt-4">
+                            <InputLabel htmlFor="password" value="Contraseña" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel
-                        htmlFor="email"
-                        className="mt-2"
-                        value="Correo Electrónico"
-                    />
+                            <TextInput
+                                id="password"
+                                type="password"
+                                name="password"
+                                value={data.password}
+                                className="mt-1 block w-full"
+                                autoComplete="current-password"
+                                onChange={(e) => setData('password', e.target.value)}
+                            />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData("email", e.target.value)}
-                    />
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+                            <InputError message={errors.password} className="mt-2" />
+                        </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Contraseña" />
+                        <div className="block mt-4">
+                            <label className="flex items-center">
+                                <Checkbox
+                                    name="remember"
+                                    checked={data.remember}
+                                    onChange={(e) => setData('remember', e.target.checked)}
+                                />
+                                <span className="ml-2 text-sm text-gray-600">Recordarme</span>
+                            </label>
+                        </div>
+                        <div className="flex justify-center items-center mt-2 ">
+                            <PrimaryButton className='bg-blue-800 hover:bg-sky-600' disabled={processing}>
+                                Iniciar Sesión
+                            </PrimaryButton>
+                        </div>
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData("password", e.target.value)}
-                    />
+                        <div className="flex space-x-4 items-center justify-center mt-4 sm:space-x-2">
+                                <Link
+                                    href={route('register')}
+                                    className="text-sm text-gray-700 hover:text-blue-800 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                        Crear cuenta
+                                </Link>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+                            {canResetPassword && (
+                                <Link
+                                    href={route('password.request')}
+                                    className="text-sm text-gray-700 hover:text-blue-800 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    ¿Olvidaste tu contraseña?
+                                </Link>
+                            )}
+                        </div>
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData("remember", e.target.checked)
-                            }
-                        />
-                        <span className="ml-2 text-sm text-gray-600">
-                            Recordarme
-                        </span>
-                    </label>
-                </div>
-                <div className="flex justify-center items-center mt-2 ">
-                    <PrimaryButton
-                        className="bg-blue-800 hover:bg-sky-600"
-                        disabled={processing}
-                    >
-                        Iniciar Sesión
-                    </PrimaryButton>
-                </div>
-
-                <div className="flex space-x-4 items-center justify-center mt-4 sm:space-x-2">
-                    <Link
-                        href={route("register")}
-                        className="text-sm text-gray-700 hover:text-blue-800 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Crear cuenta
-                    </Link>
-
-                    {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="text-sm text-gray-700 hover:text-blue-800 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            ¿Olvidaste tu contraseña?
-                        </Link>
-                    )}
-                </div>
-            </form>
-            }
-        </GuestLayout>
+                    </form>
+                </GuestLayout>
+            </Grid>
+        </Grid>
     );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -7,10 +7,11 @@ import TextInput from '@/Components/TextInput';
 import { Head, Link, useForm } from '@inertiajs/react';
 import SocialButton from '@/Components/SocialButton';
 import { useState } from 'react';
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
 
 export default function Register() {
-
-    const [showForm, setShowForm] = useState(false);
 
     const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
@@ -18,10 +19,6 @@ export default function Register() {
         password: '',
         password_confirmation: '',
     });
-
-    const ShowEmailForm = () => {
-        setShowForm(true);
-    }
 
     useEffect(() => {
         return () => {
@@ -36,121 +33,118 @@ export default function Register() {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
+        <Grid container component="main" sx={{ height: '100vh' }}>
+            <CssBaseline />
+                <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: 'url(https://e0.pxfuel.com/wallpapers/149/259/desktop-wallpaper-web-development.jpg)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+                />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <GuestLayout>
+                    <Head title="Register" />
 
-            <div className="flex flex-col">
-                <a href='/auth/github/redirect' className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        type="github"
-                        label="Usar GitHub"
-                    />
-                </a>
-                <a href='/auth/google/redirect' className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        type="google"
-                        label="Usar Google"
-                    />
-                </a>
-                <a href='/auth/facebook/redirect' className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        type="facebook"
-                        label="Usar Facebook"
-                    />
-                </a>
+                    <div className="flex flex-col">
+                        <a href='/auth/github/redirect' className='flex w-full justify-center items-center'>
+                            <SocialButton
+                                type="github"
+                                label="Usar GitHub"
+                            />
+                        </a>
+                        <a href='/auth/google/redirect' className='flex w-full justify-center items-center'>
+                            <SocialButton
+                                type="google"
+                                label="Usar Google"
+                            />
+                        </a>
 
-                <a onClick= {ShowEmailForm} className='flex w-full justify-center items-center'>
-                    <SocialButton
-                        type="email"
-                        label="Usar mi correo"
-                    />
-                </a>
-            </div>
-
-            {showForm &&
-                <form onSubmit={submit}>
-                    <div>
-                        <InputLabel htmlFor="name" value="Nombre" />
-
-                        <TextInput
-                            id="name"
-                            name="name"
-                            value={data.name}
-                            className="mt-1 block w-full"
-                            autoComplete="name"
-                            isFocused={true}
-                            onChange={(e) => setData('name', e.target.value)}
-                            required
-                        />
-
-                        <InputError message={errors.name} className="mt-2" />
+                        <a className='flex w-full justify-center items-center'>
+                            <SocialButton
+                                type="facebook"
+                                label="Usar Facebook"
+                            />
+                        </a>
                     </div>
-
-                    <div className="mt-4">
-                        <InputLabel htmlFor="email" value="Correo Electronico" />
-
-                        <TextInput
-                            id="email"
-                            type="email"
-                            name="email"
-                            value={data.email}
-                            className="mt-1 block w-full"
-                            autoComplete="username"
-                            onChange={(e) => setData('email', e.target.value)}
-                            required
-                        />
-
-                        <InputError message={errors.email} className="mt-2" />
-                    </div>
-
-                    <div className="mt-4">
-                        <InputLabel htmlFor="password" value="Contraseña" />
-
-                        <TextInput
-                            id="password"
-                            type="password"
-                            name="password"
-                            value={data.password}
-                            className="mt-1 block w-full"
-                            autoComplete="new-password"
-                            onChange={(e) => setData('password', e.target.value)}
-                            required
-                        />
-
-                        <InputError message={errors.password} className="mt-2" />
-                    </div>
-
-                    <div className="mt-4">
-                        <InputLabel htmlFor="password_confirmation" value="Confirmar contraseña" />
-
-                        <TextInput
-                            id="password_confirmation"
-                            type="password"
-                            name="password_confirmation"
-                            value={data.password_confirmation}
-                            className="mt-1 block w-full"
-                            autoComplete="new-password"
-                            onChange={(e) => setData('password_confirmation', e.target.value)}
-                            required
-                        />
-
-                        <InputError message={errors.password_confirmation} className="mt-2" />
-                    </div>
-
-                    <div className="flex items-center justify-end mt-4">
-                        <Link
-                            href={route('login')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            ¿Tienes cuenta?
-                        </Link>
-
-                        <PrimaryButton className="ml-4" disabled={processing}>
-                            Registrarse
-                        </PrimaryButton>
-                    </div>
-                </form>
-            }
-        </GuestLayout>
+                    <form onSubmit={submit}>
+                        <div>
+                            <InputLabel htmlFor="name" value="Nombre" />
+                            <TextInput
+                                id="name"
+                                name="name"
+                                value={data.name}
+                                className="mt-1 block w-full"
+                                autoComplete="name"
+                                isFocused={true}
+                                onChange={(e) => setData('name', e.target.value)}
+                                required
+                            />
+                            <InputError message={errors.name} className="mt-2" />
+                        </div>
+                        <div className="mt-4">
+                            <InputLabel htmlFor="email" value="Correo Electronico" />
+                            <TextInput
+                                id="email"
+                                type="email"
+                                name="email"
+                                value={data.email}
+                                className="mt-1 block w-full"
+                                autoComplete="username"
+                                onChange={(e) => setData('email', e.target.value)}
+                                required
+                            />
+                            <InputError message={errors.email} className="mt-2" />
+                        </div>
+                        <div className="mt-4">
+                            <InputLabel htmlFor="password" value="Contraseña" />
+                            <TextInput
+                                id="password"
+                                type="password"
+                                name="password"
+                                value={data.password}
+                                className="mt-1 block w-full"
+                                autoComplete="new-password"
+                                onChange={(e) => setData('password', e.target.value)}
+                                required
+                            />
+                            <InputError message={errors.password} className="mt-2" />
+                        </div>
+                        <div className="mt-4">
+                            <InputLabel htmlFor="password_confirmation" value="Confirmar contraseña" />
+                            <TextInput
+                                id="password_confirmation"
+                                type="password"
+                                name="password_confirmation"
+                                value={data.password_confirmation}
+                                className="mt-1 block w-full"
+                                autoComplete="new-password"
+                                onChange={(e) => setData('password_confirmation', e.target.value)}
+                                required
+                            />
+                            <InputError message={errors.password_confirmation} className="mt-2" />
+                        </div>
+                        <div className="flex items-center justify-end mt-4">
+                            <Link
+                                href={route('login')}
+                                className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                ¿Tienes cuenta?
+                            </Link>
+                            <PrimaryButton className="ml-4" disabled={processing}>
+                                Registrarse
+                            </PrimaryButton>
+                        </div>
+                    </form>
+                </GuestLayout>
+            </Grid>
+        </Grid>
     );
 }
