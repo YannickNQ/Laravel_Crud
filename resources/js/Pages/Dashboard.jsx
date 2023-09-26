840ff3f63a4225367376515b7e6f5a7254e83b1e
Yannick Axel Nina Quispe <yninaq@est.unap.edu.pe>
2023-09-26 11:17:16 -0500

    Dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-// import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import MUIDataTable from "mui-datatables";
 
@@ -17,11 +17,11 @@ const options = {
 
 export default function Dashboard({ auth }) {
     return (
-        // <AuthenticatedLayout
-            // user={auth.user}
-            // header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        // >
-            // <Head title="Dashboard" />
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        >
+            <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -36,6 +36,6 @@ export default function Dashboard({ auth }) {
                     </div>
                 </div>
             </div>
-        // </AuthenticatedLayout>
+        </AuthenticatedLayout>
     );
 }
