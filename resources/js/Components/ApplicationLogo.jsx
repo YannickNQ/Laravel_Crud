bc3ad5390d7cacaca7e04a8da50a32bb0550093f
Yannick Axel Nina Quispe <yninaq@est.unap.edu.pe>
2023-09-28 19:18:14 -0500

    Add landing page

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,31 +1,55 @@
 export default function ApplicationLogo(props) {
     return (
-        <svg {...props} viewBox="0 0 350 210" xmlns="http://www.w3.org/2000/svg">
-            <path d="M270.611 0H347.111C347.111 0 340.373 44.7929 321.611 66C299.29 91.2288 237.111 121 237.111 121L270.611 0Z" fill="url(#paint0_linear_10_31)"/>
-            <path d="M183.135 3L237.112 121C237.112 121 69.6109 149 56.6109 165.5C43.6108 182 183.135 3 183.135 3Z" fill="url(#paint1_linear_10_31)"/>
-            <path d="M74.1594 200C86.6594 141 289.611 161.5 323.659 91.9998C357.708 22.4997 276.659 237 276.659 237H86.6594C86.6594 237 61.6594 259 74.1594 200Z" fill="url(#paint2_linear_10_31)"/>
-            <path d="M189.489 196L216.111 240.629C216.111 240.629 166.881 235.497 160.821 240.629C154.761 245.76 189.489 196 189.489 196Z" fill="white"/>
-            <path d="M0 21.1958L76.5187 12.6003C76.5187 12.6003 144.146 139.846 131.724 141.242C119.302 142.637 74.2703 162.79 68.994 183.005C63.7176 203.221 0 21.1958 0 21.1958Z" fill="url(#paint3_linear_10_31)"/>
+        // Aqua Logo
+        // <svg {...props} viewBox="0 0 350 210" xmlns="http://www.w3.org/2000/svg">
+        //     <path d="M270.611 0H347.111C347.111 0 340.373 44.7929 321.611 66C299.29 91.2288 237.111 121 237.111 121L270.611 0Z" fill="url(#paint0_linear_10_31)"/>
+        //     <path d="M183.135 3L237.112 121C237.112 121 69.6109 149 56.6109 165.5C43.6108 182 183.135 3 183.135 3Z" fill="url(#paint1_linear_10_31)"/>
+        //     <path d="M74.1594 200C86.6594 141 289.611 161.5 323.659 91.9998C357.708 22.4997 276.659 237 276.659 237H214L191.5 200L163 237H86.6594C86.6594 237 61.6594 259 74.1594 200Z" fill="url(#paint2_linear_10_31)"/>
+        //     <path d="M0 21.1958L76.5187 12.6003C76.5187 12.6003 144.146 139.846 131.724 141.242C119.302 142.637 74.2703 162.79 68.994 183.005C63.7176 203.221 0 21.1958 0 21.1958Z" fill="url(#paint3_linear_10_31)"/>
+        //     <defs>
+        //     <linearGradient id="paint0_linear_10_31" x1="292.111" y1="0" x2="292.111" y2="121" gradientUnits="userSpaceOnUse">
+        //     <stop offset="0.151042" stopColor="#13B7DC"/>
+        //     <stop offset="0.96875" stopColor="#2D4A71"/>
+        //     </linearGradient>
+        //     <linearGradient id="paint1_linear_10_31" x1="146.434" y1="3" x2="146.434" y2="166.574" gradientUnits="userSpaceOnUse">
+        //     <stop stopColor="#13B7DC"/>
+        //     <stop offset="0.895833" stopColor="#2C3E64"/>
+        //     </linearGradient>
+        //     <linearGradient id="paint2_linear_10_31" x1="201.336" y1="78.1409" x2="201.336" y2="241.015" gradientUnits="userSpaceOnUse">
+        //     <stop offset="0.151042" stopColor="#13B7DC"/>
+        //     <stop offset="0.96875" stopColor="#2D4A71"/>
+        //     </linearGradient>
+        //     <linearGradient id="paint3_linear_10_31" x1="59.331" y1="14.531" x2="78.299" y2="183.387" gradientUnits="userSpaceOnUse">
+        //     <stop offset="0.151042" stopColor="#13B7DC"/>
+        //     <stop offset="0.96875" stopColor="#2D4A71"/>
+        //     </linearGradient>
+        //     </defs>
+        // </svg>
+
+        //Fire Logo
+        <svg {...props} viewBox="0 0 350 210" xmlns="http://www.w3.org/2000/svg">            <path d="M270.611 0H347.111C347.111 0 340.373 44.7929 321.611 66C299.29 91.2288 237.111 121 237.111 121L270.611 0Z" fill="url(#paint0_linear_18_2)"/>
+            <path d="M183.135 3L237.112 121C237.112 121 69.6109 149 56.6109 165.5C43.6108 182 183.135 3 183.135 3Z" fill="url(#paint1_linear_18_2)"/>
+            <path d="M74.1594 200C86.6594 141 289.611 161.5 323.659 91.9998C357.708 22.4997 276.659 237 276.659 237H214L191.5 200L163 237H86.6594C86.6594 237 61.6594 259 74.1594 200Z" fill="url(#paint2_linear_18_2)"/>
+            <path d="M0 21.1958L76.5187 12.6003C76.5187 12.6003 144.146 139.846 131.724 141.242C119.302 142.637 74.2703 162.79 68.994 183.005C63.7176 203.221 0 21.1958 0 21.1958Z" fill="url(#paint3_linear_18_2)"/>
             <defs>
-            <linearGradient id="paint0_linear_10_31" x1="292.111" y1="0" x2="292.111" y2="121" gradientUnits="userSpaceOnUse">
-            <stop offset="0.151042" stopColor="#13B7DC"/>
-            <stop offset="0.96875" stopColor="#2D4A71"/>
+            <linearGradient id="paint0_linear_18_2" x1="292.111" y1="0" x2="292.111" y2="121" gradientUnits="userSpaceOnUse">
+            <stop stopColor="#EFE72D"/>
+            <stop offset="0.740371" stopColor="#EF2D2D"/>
             </linearGradient>
-            <linearGradient id="paint1_linear_10_31" x1="146.434" y1="3" x2="146.434" y2="166.574" gradientUnits="userSpaceOnUse">
-            <stop stopColor="#13B7DC"/>
-            <stop offset="0.895833" stopColor="#2C3E64"/>
+            <linearGradient id="paint1_linear_18_2" x1="146.434" y1="3" x2="146.434" y2="166.574" gradientUnits="userSpaceOnUse">
+            <stop offset="0.0545083" stopColor="#EFC42D"/>
+            <stop offset="0.74175" stopColor="#EF2F2D"/>
             </linearGradient>
-            <linearGradient id="paint2_linear_10_31" x1="201.336" y1="78.1409" x2="201.336" y2="241.015" gradientUnits="userSpaceOnUse">
-            <stop offset="0.151042" stopColor="#13B7DC"/>
-            <stop offset="0.96875" stopColor="#2D4A71"/>
+            <linearGradient id="paint2_linear_18_2" x1="201.336" y1="78.1409" x2="201.336" y2="241.015" gradientUnits="userSpaceOnUse">
+            <stop offset="0.396538" stopColor="#EFB82D"/>
+            <stop offset="0.96875" stopColor="#EF2D2D"/>
             </linearGradient>
-            <linearGradient id="paint3_linear_10_31" x1="59.331" y1="14.531" x2="78.299" y2="183.387" gradientUnits="userSpaceOnUse">
-            <stop offset="0.151042" stopColor="#13B7DC"/>
-            <stop offset="0.96875" stopColor="#2D4A71"/>
+            <linearGradient id="paint3_linear_18_2" x1="59.331" y1="14.531" x2="78.299" y2="183.387" gradientUnits="userSpaceOnUse">
+            <stop offset="0.151042" stopColor="#EF722D"/>
+            <stop offset="0.96875" stopColor="#EF2D2D"/>
             </linearGradient>
             </defs>
-
         </svg>
+        
     );
-
 }
---
+++ resources/js/Layouts/TypeWriterLayout.jsx
@@ -0,0 +1,41 @@
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import { Link } from '@inertiajs/react';
+import { TypeAnimation } from 'react-type-animation';
+
+export default function TypeWriter({ children }) {
+    return (
+        <div className="relative hidden flex-1 flex-col justify-center px-5 pt-8 text-white bg-[url('https://e0.pxfuel.com/wallpapers/149/259/desktop-wallpaper-web-development.jpg')] bg-cover bg-no-repeat md:flex md:py-[22px] lg:px-8">
+                    <nav className="left-0 top-8 flex w-full px-6 sm:absolute md:top-[22px] md:px-6 lg:px-8 ">
+                        <h1 aria-label='WebDevelopers'>
+                            <div>
+                                <Link href="/" className='flex cursor-default items-end text-[20px] font-bold leading-none lg:text-[22px]'>
+                                    <ApplicationLogo className="w-16 h-16 fill-current" />
+                                    <span className='text-white text-3xl font-bold'>ebDevOps</span>
+                                </Link>
+                            </div>                        
+                        </h1>
+                    </nav>
+                    <div className="flex flex-col text-[32px] leading-[1.2] md:text-[40px]" aria-hidden="true">
+                        <TypeAnimation
+                        sequence={[
+                            'Somos estudiantes apasionados, explorando el vasto mundo del desarrollo web.', 
+                            1000,
+                            'Transformamos ideas en aplicaciones web interactivas con React.',
+                            1000,
+                            'Con Laravel, la elegancia y la funcionalidad se unen en cada proyecto.',
+                            1000,
+                            'Inertia nos permite construir rápido, sin sacrificar la simplicidad.',
+                            1000,
+                            'Con Tailwind, nuestro diseño web es tan elegante como funcional.',
+                            1000,
+                            'Creamos experiencias de usuario excepcionales con Material UI.',
+                            1000
+                        ]}
+                        wrapper='span'
+                        speed={50}
+                        repeat={Infinity}
+                        />
+                    </div>
+                </div>
+    );
+}
