2f03a61f96e7c8a5b14feb800bad362bcb94a338
Yannick Axel Nina Quispe <yninaq@est.unap.edu.pe>
2023-09-28 20:19:47 -0500

    GuestLayout Title

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,7 +5,7 @@ export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div className="w-full sm:max-w-md mt-6 px-6 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <span className='text-2xl font-semibold w-full sm:max-w-md px-6 py-4 overflow-hidden sm:rounded-lg flex items-center text-center justify-center '>Inicia sesión o regístrate en cuestión de segundos</span>
+                <span className='text-xl font-semibold w-full sm:max-w-md px-6 py-4 overflow-hidden sm:rounded-lg flex items-center text-center justify-center '>Inicia sesión o regístrate en cuestión de segundos</span>
                 <span className='text-base'>Usa tu correo electrónico u otro servicio para acceder a Canva gratis.</span>
                 {children}
             </div>
